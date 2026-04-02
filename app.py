685f591a2ec6cb2d0e72b708275f9201b299acde
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-02 11:01:15 +0530

    Create Class

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 
-
+class ImageToPDFConverterApp:
+    def __init__(self, root):
+        self.root = root
+        self.image_paths = []
+        self.output_pdf_name = tk.StringVar()
+        self.selected_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 
 
 
