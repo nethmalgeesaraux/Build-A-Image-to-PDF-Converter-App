87b5dea6a1984830e903014b9d360681dc057ef1
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-02 11:34:46 +0530

    add PIL library and messagebox module for image processing and user alerts

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import os
+from PIL import Image
 
 class ImageToPDFConverterApp:
     def __init__(self, root):
